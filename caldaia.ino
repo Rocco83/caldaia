956103069c25ef22e7d494e8cc9720b36f59774b
Daniele Palumbo <daniele@retaggio.net>
2022-01-27 02:18:58 +0100

    Working and cleaned up version of caldaia.ino
    Working only via serial port currently

--- caldaia.ino
+++ caldaia.ino
@@ -1,64 +1,45 @@
-// File M32_Test
+// File M32 Sensor MQTT client
 //
-// A Arduino program to test the operation of pressure sensors in the TE Connectivity M32 series (I2C versions only)
+// A Arduino program to send the data (pressure and temperature) from the pressure sensors TE Connectivity M32 series (I2C versions only)
+// Original file for MS4525 pressure sensor
+// https://forum.arduino.cc/t/ms-4525do/298848/14
+// other useful link:
+// https://forum.arduino.cc/t/ms-4525do/298848/5
+// https://forum.arduino.cc/t/pressure-sensor-i2c-communication/853443/2
 //
 #include <Wire.h> // Arduino I2C library
 #include <stdint.h> // Standard C, Allows explicit data type declaration.
 
-//////////////////////////////////////////////////////////////////////////////////////
-// M32 sensor characteristics (from the 4/2019 version of the data sheet)
-//////////////////////////////////////////////////////////////////////////////////////
+/*
+ * M32 sensor characteristics (from the 02/2021 version of the data sheet)
+ */
 
-// M32 sensor I2C address (uncomment the Interface Type of the device you are using)
-// Interface Type I
+// M32 sensor I2C address
 const uint8_t M32Address = 0x28;
-// Interface Type J
-//const uint8_t M32Address = 0x36;
-// Interface Type K
-//const uint8_t M32Address = 0x46;
-// Interface Type 0
-//const uint8_t M32Address = 0x48;
 
 // M32 sensor full scale range and units
-//const int16_t M32FullScaleRange = 1; // 1 psi
 const int16_t M32FullScaleRange = 6.89476; // 100 psi in bar
-//const int16_t M32FullScaleRange = 0.0689476; // 1 psi in Bar
-//const int16_t M32FullScaleRange = 6895; // 1 psi in Pascal
-//const int16_t M32FullScaleRange = 2; // 2 psi
-//const int16_t M32FullScaleRange = 5; // 5 psi
 
-// M32 Sensor type (A or B) comment out the wrong type assignments
-// Type A (10% to 90%)
-//const int16_t M32MinScaleCounts = 1638;
-//const int16_t M32FullScaleCounts = 14746;
-// Type B (5% to 95%)
-//const int16_t M32MinScaleCounts = 819;
-//const int16_t M32FullScaleCounts = 15563;
-// M32 ??
+// M32 sensor, see PERFORMANCE SPECIFICATION (DIGITAL). Could be to be adjusted
 const int16_t M32MinScaleCounts = 1000;
 const int16_t M32FullScaleCounts = 15000;
 const int16_t M32Span=M32FullScaleCounts-M32MinScaleCounts;
 
-//M32 sensor pressure style, differential or otherwise. Comment out the wrong one.
-//Differential
+// M32 sensor pressure style, gauge or differential. Comment out the wrong one.
+// Differential
 //const int16_t M32ZeroCounts=(M32MinScaleCounts+M32FullScaleCounts)/2;
 // Absolute, Gauge
 const int16_t M32ZeroCounts=M32MinScaleCounts;
 
-//////////////////////////////////////////////////////////////////////////////////////
-// end of M32 sensor characteristics
-//////////////////////////////////////////////////////////////////////////////////////
+/*
+ * end of M32 sensor characteristics
+ */
 
+// functions for debugging and printing
 
+// function to print the bits of a variable
 void printBinary(int number, uint8_t Length){
   static int bits;
-  // debug
-  /*Serial.flush();
-  Serial.println("");
-  Serial.print("starting loop: ");
-  Serial.println(number);
-  Serial.print("bits = ");
-  Serial.println(bits);*/
   if ( bits > 64 ) {
     // bits cant reach 64
     Serial.print(F("64 loops done, failed somewhere. Debug: bits:"));
@@ -77,13 +58,11 @@ void printBinary(int number, uint8_t Length){
     printBinary(number >> 1, Length); // Remove a bit and do a recursive call this function.
     if (bits) for (uint8_t x = (Length - bits); x; x--)Serial.write('0'); // Add the leading zeros
     bits = 0; // clear no need for this any more
-    // debug
-    //Serial.flush();
     Serial.write((number & 1) ? '1' : '0'); // print the bits in reverse order as we depart the recursive function
   }
 }
 
-
+// function to write the float/double value to Serial (seems unneeded)
 char* tempToAscii(float temp)
 // convert long to type char and store in variable array ascii
 {
@@ -105,10 +84,10 @@ char* tempToAscii(float temp)
   return ascii;
 }
 
-// fetch_i2cdata is a function to do the I2C read and extraction of the three data fields
-//
-byte fetch_i2cdata(double &pressure, double &temperature)
+// fetch_i2cdata is the core function to do the I2C read and extraction of the three data fields
+byte fetch_i2cdata(double &pressure, double &temperature, byte rawdata[])
 {
+uint8_t buffersize;
 byte _status;
 byte Press_H;
 byte Press_L;
@@ -118,51 +97,96 @@ byte Temp_L;
 uint16_t P_dat; // 14 bit pressure data
 uint16_t T_dat; // 11 bit temperature data
 
-Wire.requestFrom(M32Address, static_cast<uint8_t>(4), static_cast<uint8_t>(true)); //Request 4 bytes, 2 pressure/status and 2 temperature
-Press_H = Wire.read();
-Press_L = Wire.read();
-Temp_H = Wire.read();
-Temp_L = Wire.read();
+// wake sensor
+// if not performed, 1 reading every 2 is stale
+Wire.requestFrom(0x28, 0);
+// read data
+// the casting is needed because Wire.requestFrom require int OR uint_8t, not a mix of such
+// a previous version was asking also for stop=true, but is creating Stale issue along with the above wake sensor
+buffersize = Wire.requestFrom(M32Address, static_cast<uint8_t>(4)); //Request 4 bytes, 2 pressure/status and 2 temperature
+// buffersize = 4 expected, otherwise it's an error
+if ( buffersize != 4 ) {
+  return 4;
+}
+rawdata[0] = Wire.read();
+rawdata[1] = Wire.read();
+rawdata[2] = Wire.read();
+rawdata[3] = Wire.read();
 
-// print raw data
-printBinary(Press_H, 8);
-Serial.println("\n");
-printBinary(Press_L, 8);
-Serial.println("\n");
-printBinary(Temp_H, 8);
-Serial.println("\n");
-printBinary(Temp_L, 8);
-Serial.println("\n");
+// as the variable Press_H/L, Temp_H/L were meant to be modified, have to save the rawdata into another variable
+Press_H = rawdata[0];
+Press_L = rawdata[1];
+Temp_H = rawdata[2];
+Temp_L = rawdata[3];
 
+// get the return code, first 2 bit of the first i2c packet
+// 00: OK
+// 01: Reserved
+// 10: Stale
+// 11: Error
 _status = (Press_H >> 6) & 0x03;
+
+// keep in Press_H only the meaningful data for pressure, first byte
 Press_H = Press_H & 0x3f;
+// combine Press_H and Press_L to get the 14 bit precision
 P_dat = (((uint16_t)Press_H) << 8 ) | Press_L; // space added after ‘8’ so code can be uploaded without emoji conversion!
 
-// the low part of the temperature must remove the last 5 bits
+// the low part of the temperature must remove the last 5 bits, as that are always 0 (unused)
 Temp_L = (Temp_L >> 5);
 // the correct temperature must take the high part, adding 3 bit on the end, and then the remaining 3 bit on Temp_L
 T_dat = (((uint16_t)Temp_H) << 3) | Temp_L;
 
-// debug
-Serial.print("pressure (binary): ");
+// Print the data on Serial for monitoring (unneeded, anyhow)
+
+switch (_status)
+{
+case 0:
+//Serial.println("Ok ");
+break;
+case 1:
+Serial.println("M32 i2c Busy 01");
+break;
+case 2:
+Serial.println("M32 i2c Slate 10");
+break;
+default:
+Serial.println("M32 i2c Error 11");
+break;
+}
+
+// print raw data
+Serial.println("raw data. S: Status, P: Pressure, T: Temperature, x: unused");
+Serial.println("SSPPPPPP PPPPPPPP TTTTTTTT TTTxxxxx");
+printBinary(rawdata[0], 8);
+Serial.print(" ");
+printBinary(rawdata[1], 8);
+Serial.print(" ");
+printBinary(rawdata[2], 8);
+Serial.print(" ");
+printBinary(rawdata[3], 8);
+Serial.println("\n");
+
+// Print debug data for pressure
+Serial.print("raw pressure (dec count): ");
+Serial.print(P_dat);
+Serial.print(", ");
+Serial.print("raw pressure (binary): ");
 printBinary(P_dat, 14);
 Serial.println("\n");
 
-Serial.print("pressure (dec): ");
-Serial.print(P_dat);
-Serial.println("\n");
-
+// Print debug data for temperature 
+Serial.print("raw temperature (dec count): ");
+Serial.print(T_dat);
+Serial.print(", ");
 Serial.print("temperature (binary): ");
 printBinary(T_dat, 11);
 Serial.println("\n");
 
+// make the math on pressure
+// static_cast<double> is needed for each variable to avoid that Arduino decide to make use of int or such
+pressure=(static_cast<double>(static_cast<double>(P_dat)-M32ZeroCounts))/static_cast<double>(M32Span)*static_cast<double>(M32FullScaleRange);
 
-Serial.print("temperature (dec): ");
-Serial.print(T_dat);
-Serial.println("\n");
-
-pressure=(static_cast<double>(static_cast<int16_t>(P_dat)-M32ZeroCounts))/static_cast<int16_t>(M32Span)*static_cast<int16_t>(M32FullScaleRange);
-
+// Original formula in the datasheet
 // output (decimal counts) = ( output °C + 50°C ) * 2048 / (150°C - (-50°C))
 /* output °C	Digital counts (Dec)	Digital counts
    0		 512			0x200
@@ -170,20 +194,19 @@ pressure=(static_cast<double>(static_cast<int16_t>(P_dat)-M32ZeroCounts))/static
   25		 767			0x2FF
   40		 921			0x399
   55		1075			0x433 */
-//temperature=(T_dat+50)*2048/200;
 // reversed formula from datasheet as we have count and want temperature
-temperature = ( static_cast<int16_t>(T_dat) * 200 / 2048 ) - 50;
-temperature = T_dat + 50;
-Serial.println(T_dat);
-temperature = (double)798*200;
-Serial.println(temperature);
+// output °C = ( output (decimal counts) / 2048 * 200 ) - 50
+// without static_cast<double> the values are mocked (eg 798*200 = 28528, while should be 153600
+temperature = ( static_cast<double>(T_dat) * static_cast<double>(200) / static_cast<double>(2048) ) - static_cast<double>(50);
 
-temperature = 798*200/2048;
-Serial.println(temperature);
+// Finally print converted data
 
+Serial.print("pressure bar: ");
+Serial.println(pressure);
+Serial.print("temperature °C: ");
+Serial.println(temperature);
 
 return _status;
-
 }
 
 // setup is the main function to setup the diagnostic serial port and the I2C port
@@ -193,6 +216,7 @@ void setup()
 Serial.begin(115200);
 Wire.begin();
 // wait until serial port opens for native USB devices
+// Arduino Uno/Ethernet/Mega does not need it (no CDC serial)
 while (! Serial)
 {
 delay(1);
@@ -202,37 +226,17 @@ Serial.println("M32 test");
 
 }
 
+// main()
 void loop()
 {
-
+// variabled defined here and passed as reference to fetch_i2cdata
 byte _status; // A two bit field indicating the status of the I2C read
-
 double pressure;
 double temperature;
+byte rawdata[4];
 
-_status = fetch_i2cdata(pressure, temperature);
+_status = fetch_i2cdata(pressure, temperature, rawdata);
 
-switch (_status)
-{
-case 0:
-//Serial.println("Ok ");
-break;
-case 1:
-Serial.println("Busy");
-break;
-case 2:
-Serial.println("Slate");
-break;
-default:
-Serial.println("Error");
-break;
-}
-
-Serial.print("pressure:");
-Serial.println(pressure);
-
-Serial.print("°C:");
-Serial.println(temperature);
 
 delay(1000);
 
