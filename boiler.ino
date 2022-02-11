4dca1a97ea89ea8455ed2992d387092e7736dda8
Daniele Palumbo <daniele@retaggio.net>
2022-02-11 12:58:06 +0100

    staging

--- boiler.ino
+++ boiler.ino
@@ -17,22 +17,11 @@
 // variable for the local project
 #include "boiler.h"
 
-/*
- * MQTT settings
- */
-// it's a public name space, so open the key & secret value.
-// if you want to make a privte one, you should make a private namespace on http://shiftr.io
-//#define MQTT_DeviceName "arduino_boiler"
-//#define MQTT_Username "boiler"
-//#define MQTT_Password "boiler"
-//#define MQTT_topic_Message  "/boiler"
-
 // tested up to 163 chars
-// {"temperature":23.83,"temperature_unit":"°C","pressure":1.11,"pressure_unit":"bar","uptime":4294967000,"mqttRawData":["00000011","11001001","01011110","10000000"]}
+// {"temperature":23.83,"temperature_unit":"°C","pressure":1.11,"pressure_unit":"bar","uptime":4294967000,"binData":["00000011","11001001","01011110","10000000"]}
 // Sketch uses 32180 bytes (99%) of program storage space. Maximum is 32256 bytes.
 // Global variables use 1063 bytes (51%) of dynamic memory, leaving 985 bytes for local variables. Maximum is 2048 bytes.
-#define BUFFERSIZE 200
-
+#define BUFFERSIZE 180
 
 /*
  * M32 sensor characteristics (from the 02/2021 version of the data sheet)
@@ -43,7 +32,7 @@
 #define M32Address 0x28
 
 // M32 sensor full scale range and units
-const int16_t M32FullScaleRange = 6.89476; // 100 psi in bar
+#define M32FullScaleRange 6.89476 // 100 psi in bar
 
 // M32 sensor, see PERFORMANCE SPECIFICATION (DIGITAL). Could be to be adjusted
 //const int16_t M32MinScaleCounts PROGMEM = 1000;
@@ -75,24 +64,22 @@ IPAddress ip;
 uint8_t ipObtained = false;
 
 MQTTClient mqttClient(BUFFERSIZE);
-//#define MQTTBROKER "mqtt.example.com"
-//#define MQTTPORT = 1883
-//#define MQTTPREFIX ""
 
 unsigned long now = millis();
 
 
 // time in ms expected to run the sketch
-#define RUNTIME 48
+//#define RUNTIME 48
 
 // functions for debugging and printing
 
 // function to print the bits of a variable
+/* debug
 void printBinary(int number, uint8_t Length){
   static int bits;
   if ( bits > 64 ) {
     // bits cant reach 64
-    Serial.print(F("64 loops done, failed somewhere. Debug: bits:"));
+    Serial.print(F("64 loops done, failed. bits:"));
     Serial.print(bits);
     Serial.print(F(" lenght: "));
     Serial.print(Length);
@@ -106,7 +93,7 @@ void printBinary(int number, uint8_t Length){
   if (number) { //The remaining bits have a value greater than zero continue
     bits++; // Count the number of bits so we know how many leading zeros to print first
     printBinary(number >> 1, Length); // Remove a bit and do a recursive call this function.
-    if (bits) for (uint8_t x = (Length - bits); x; x--)Serial.write('0'); // Add the leading zeros
+    if (bits) for (uint8_t x = (Length - bits); x; x--) Serial.write('0'); // Add the leading zeros
     bits = 0; // clear no need for this any more
     Serial.write((number & 1) ? '1' : '0'); // print the bits in reverse order as we depart the recursive function
   } else {
@@ -118,8 +105,9 @@ void printBinary(int number, uint8_t Length){
     }
   }
 }
+*/
 
-char * int2bin(int x)
+char * int2bin(uint8_t x)
 {
   static char buffer[9];
   for (int i=0; i<8; i++) buffer[7-i] = '0' + ((x & (1 << i)) > 0);
@@ -127,37 +115,38 @@ char * int2bin(int x)
   return buffer;
 }
 
+
 // rounds a number to 2 decimal places
 // example: round(3.14159) -> 3.14
 double round2(double value) {
    return (int)(value * 100 + 0.5) / 100.0;
 }
 byte initializeEthernet() {
-  Serial.println(F("Trying to configure Ethernet using DHCP"));
+  Serial.println(F("DHCP init"));
   if (Ethernet.begin(mac) == 0) {
-    Serial.println(F("Failed to initialize Ethernet using DHCP"));
+    Serial.println(F("DHCP init failed"));
 
     if (Ethernet.hardwareStatus() == EthernetNoHardware) {
 
-      Serial.println(F("Ethernet shield was not found.  Sorry, can't run without hardware. :("));
+      Serial.println(F("Ethernet shield was not found"));
 
     } else if (Ethernet.linkStatus() == LinkOFF) {
 
-      Serial.println(F("Ethernet cable is not connected."));
+      Serial.println(F("Ethernet cable not connected"));
     }
 
     delay(10000);
   }
-  Serial.println(F("Initialized Ethernet using DHCP"));
+  Serial.println(F("DHCP init OK"));
   // assign value to the global value that give me current status
   if ( checkDhcp() == false ) {
-    Serial.println(F("Failed to configure Ethernet using DHCP"));
+    Serial.println(F("DHCP config failed"));
     delay(1000);
   }
   ipObtained=true;
   // dhcp test end
 
-  Serial.println(F("Network configuration succeded"));
+  Serial.println(F("Network configuration OK"));
 
   // is it needed?
   byte macBuffer[6];  // create a buffer to hold the MAC address
@@ -174,7 +163,7 @@ byte initializeEthernet() {
   // print your local IP address:
   Serial.print(F("IP: "));
   Serial.println(Ethernet.localIP());
-  Serial.print(F("Gateway: "));
+  Serial.print(F("GW: "));
   Serial.println(Ethernet.gatewayIP());
   Serial.print(F("Subnet: "));
   Serial.println(Ethernet.subnetMask());
@@ -220,21 +209,21 @@ byte checkDhcp() {
 void mqttConnect() {
   Serial.print(F("Connecting to MQTT broker: "));
   Serial.println(MQTTBROKER);
-  Serial.print(F("connecting..."));
 
   while (!mqttClient.connect(MQTT_DeviceName, MQTT_Username, MQTT_Password)) {
-//  while (!mqttClient.connect(MQTT_DeviceName, MQTT_Username, MQTT_Password) || counter == 60) {
+  //while (!mqttClient.connect(MQTT_DeviceName, MQTT_Username, MQTT_Password) || counter == 60) {
     Serial.print(F("."));
     //counter++;
     delay(1000);
   }
 
-  Serial.println(F("\r\nconnected!"));
+  Serial.println(F("\r\nconnected"));
 
-  mqttClient.subscribe(MQTT_topic_Message);
+  // not needed, only send
+  //mqttClient.subscribe(MQTT_topic_Message);
 }
 
-byte MQTTSend(double &pressure, double &temperature, byte rawdata[]) {
+byte MQTTSend(double &pressure, double &temperature, uint16_t &P_dec, uint16_t &T_dec, byte rawdata[]) {
   // variable for the JSON message to MQTT
   StaticJsonDocument<BUFFERSIZE-8> mqttData;
   char json_string[BUFFERSIZE-8];
@@ -242,18 +231,24 @@ byte MQTTSend(double &pressure, double &temperature, byte rawdata[]) {
 
   // write data to mqtt variables
   mqttData["temperature"] = temperature;
-  mqttData["temperature_unit"] = "°C";
+  mqttData["temperature_unit"] = "C";
 
   mqttData["pressure"] = pressure;
   mqttData["pressure_unit"] = "bar";
 
   mqttData["uptime"] = now;
 
-  JsonArray mqttRawData = mqttData.createNestedArray("mqttRawData");
-  mqttRawData.add(int2bin(rawdata[0]));
-  mqttRawData.add(int2bin(rawdata[1]));
-  mqttRawData.add(int2bin(rawdata[2]));
-  mqttRawData.add(int2bin(rawdata[3]));
+  mqttData["p_dec"] = (uint16_t)P_dec;
+  mqttData["t_dec"] = (uint16_t)T_dec;
+
+  // removed as the memory of arduino ethernet does not allow a very long string
+  /*
+  JsonArray binData = mqttData.createNestedArray("binData");
+  binData.add(int2bin(rawdata[0]));
+  binData.add(int2bin(rawdata[1]));
+  binData.add(int2bin(rawdata[2]));
+  binData.add(int2bin(rawdata[3]));
+  */
 
   serializeJson(mqttData, json_string);
 
@@ -268,12 +263,11 @@ byte MQTTSend(double &pressure, double &temperature, byte rawdata[]) {
     mqttConnect();
   }
 
-//  mqttreturn = mqttClient.publish("/boiler", json_string);
   mqttreturn = mqttClient.publish(MQTT_topic_Message, json_string);
   if ( !mqttreturn ) {
-    Serial.println(F("MQTT send message failed"));
+    Serial.println(F("MQTT send failed"));
   } else {
-    Serial.println(F("MQTT send message OK"));
+    Serial.println(F("MQTT send OK"));
   }
 
   return mqttreturn;
@@ -282,16 +276,14 @@ byte MQTTSend(double &pressure, double &temperature, byte rawdata[]) {
 uint8_t resetStale() {
   // wake sensor
   // if not performed, 1 reading every 2 is stale
-  Serial.println(F("Read from i2c trying to remove the stale"));
+  Serial.println(F("Read from i2c, try to remove the stale"));
   delay(100*1.2);
-  //return Wire.requestFrom(M32Address, (uint8_t)0);
   return Wire.requestFrom((uint8_t)M32Address, (uint8_t)0);
-  delay(100*1.2);
 }
 
 
 // fetch_i2cdata is the core function to do the I2C read and extraction of the three data fields
-byte fetch_i2cdata(double &pressure, double &temperature, byte rawdata[]) {
+byte fetch_i2cdata(double &pressure, double &temperature, uint16_t &P_dec, uint16_t &T_dec, byte rawdata[]) {
   uint8_t buffersize;
   byte _status;
   byte Press_H;
@@ -299,13 +291,16 @@ byte fetch_i2cdata(double &pressure, double &temperature, byte rawdata[]) {
   byte Temp_H;
   byte Temp_L;
 
-  uint16_t P_dat; // 14 bit pressure data
-  uint16_t T_dat; // 11 bit temperature data
+  //uint16_t P_dec; // 14 bit pressure data
+  //uint16_t T_dec; // 11 bit temperature data
+  P_dec = 0;
+  T_dec = 0;
 
   // wake sensor
   // if not performed, 1 reading every 2 is stale
-  //Wire.requestFrom(M32Address, (uint8_t)0);
+  Serial.println(F("w"));
   Wire.requestFrom((uint8_t)M32Address, (uint8_t)0);
+  wdt_reset();
 
   // standard time for i2c is 100Khz
   // from the datasheet:
@@ -315,10 +310,14 @@ byte fetch_i2cdata(double &pressure, double &temperature, byte rawdata[]) {
   // read data
   // the casting is needed because Wire.requestFrom require int OR uint_8t, not a mix of such
   // a previous version was asking also for stop=true, but is creating Stale issue along with the above wake sensor
+  Serial.println(F("r"));
   buffersize = Wire.requestFrom((uint8_t)M32Address, static_cast<uint8_t>(4)); //Request 4 bytes, 2 pressure/status and 2 temperature
+  wdt_reset();
   // buffersize = 4 expected, otherwise it's an error
   if ( buffersize != 4 ) {
-    return 4;
+    Serial.print(F("Returned a wrong number of bytes: "));
+    Serial.println(buffersize);
+    return buffersize;
   }
   rawdata[0] = Wire.read();
   rawdata[1] = Wire.read();
@@ -341,63 +340,70 @@ byte fetch_i2cdata(double &pressure, double &temperature, byte rawdata[]) {
   // keep in Press_H only the meaningful data for pressure, first byte
   Press_H = Press_H & 0x3f;
   // combine Press_H and Press_L to get the 14 bit precision
-  P_dat = (((uint16_t)Press_H) << 8 ) | Press_L; // space added after ‘8’ so code can be uploaded without emoji conversion!
+  P_dec = (((uint16_t)Press_H) << 8 ) | Press_L; // space added after ‘8’ so code can be uploaded without emoji conversion!
 
   // the low part of the temperature must remove the last 5 bits, as that are always 0 (unused)
   Temp_L = (Temp_L >> 5);
   // the correct temperature must take the high part, adding 3 bit on the end, and then the remaining 3 bit on Temp_L
-  T_dat = (((uint16_t)Temp_H) << 3) | Temp_L;
+  T_dec = (((uint16_t)Temp_H) << 3) | Temp_L;
 
   // Print the data on Serial for monitoring (unneeded, anyhow)
-
-  switch (_status)
-  {
-  case 0:
-  //Serial.println(F("Ok "));
-  break;
-  case 1:
-  Serial.println(F("M32 i2c Busy 01"));
-  break;
-  case 2:
-  Serial.println(F("M32 i2c Slate 10"));
-  resetStale();
-  break;
-  default:
-  Serial.println(F("M32 i2c Error 11"));
-  break;
+  switch (_status) {
+    case 0:
+      //Serial.println(F("Ok "));
+      break;
+    case 1:
+      Serial.println(F("M32 i2c Busy 01"));
+      break;
+    case 2:
+      Serial.println(F("M32 i2c Slate 10"));
+      resetStale();
+      // continue with the reading that was used
+      break;
+    default:
+      Serial.println(F("M32 i2c Error 11"));
+      break;
   }
 
   // print raw data
   Serial.println(F("raw data. S: Status, P: Pressure, T: Temperature, x: unused"));
   Serial.println(F("SSPPPPPP PPPPPPPP TTTTTTTT TTTxxxxx"));
-  printBinary(rawdata[0], 8);
+  //printBinary(rawdata[0], 8);
+  Serial.print(int2bin(rawdata[0]));
   Serial.print(F(" "));
-  printBinary(rawdata[1], 8);
+  //printBinary(rawdata[1], 8);
+  Serial.print(int2bin(rawdata[1]));
   Serial.print(F(" "));
-  printBinary(rawdata[2], 8);
+  //printBinary(rawdata[2], 8);
+  Serial.print(int2bin(rawdata[2]));
   Serial.print(F(" "));
-  printBinary(rawdata[3], 8);
+  //printBinary(rawdata[3], 8);
+  Serial.print(int2bin(rawdata[3]));
   Serial.println(F("\n"));
 
   // Print debug data for pressure
   Serial.print(F("raw pressure (dec count): "));
-  Serial.print(P_dat);
+  Serial.print(P_dec);
+  /* debug
   Serial.print(F(", "));
   Serial.print(F("raw pressure (binary): "));
-  printBinary(P_dat, 14);
+  printBinary(P_dec, 14);
+  */
   Serial.println(F(""));
 
   // Print debug data for temperature
-  Serial.print(F("raw temperature (dec count): "));
-  Serial.print(T_dat);
+  Serial.print(F("raw temp (dec count): "));
+  Serial.print(T_dec);
+  /* debug
   Serial.print(F(", "));
-  Serial.print(F("temperature (binary): "));
-  printBinary(T_dat, 11);
+  Serial.print(F("temp (binary): "));
+  printBinary(T_dec, 11);
+  */
   Serial.println(F("\n"));
 
   // make the math on pressure
   // static_cast<double> is needed for each variable to avoid that Arduino decide to make use of int or such
-  pressure=round2((static_cast<double>(static_cast<double>(P_dat)-M32ZeroCounts))/static_cast<double>(M32Span)*static_cast<double>(M32FullScaleRange));
+  pressure=round2((static_cast<double>(static_cast<double>(P_dec)-M32ZeroCounts))/static_cast<double>(M32Span)*static_cast<double>(M32FullScaleRange));
 
   // Original formula in the datasheet
   // output (decimal counts) = ( output °C + 50°C ) * 2048 / (150°C - (-50°C))
@@ -410,7 +416,7 @@ byte fetch_i2cdata(double &pressure, double &temperature, byte rawdata[]) {
   // reversed formula from datasheet as we have count and want temperature
   // output °C = ( output (decimal counts) / 2048 * 200 ) - 50
   // without static_cast<double> the values are mocked (eg 798*200 = 28528, while should be 153600
-  temperature = round2(( static_cast<double>(T_dat) * static_cast<double>(200) / static_cast<double>(2048) ) - static_cast<double>(50));
+  temperature = round2(( static_cast<double>(T_dec) * static_cast<double>(200) / static_cast<double>(2048) ) - static_cast<double>(50));
 
   // Finally print converted data
 
@@ -419,6 +425,8 @@ byte fetch_i2cdata(double &pressure, double &temperature, byte rawdata[]) {
   Serial.print(F("temperature °C: "));
   Serial.println(temperature);
 
+  Serial.print(F("status: "));
+  Serial.println(_status);
   return _status;
 }
 
@@ -430,6 +438,10 @@ void setup()
   Ethernet.init(10);  // Most Arduino shields
   Serial.begin(115200);
   Wire.begin();
+  // https://forum.arduino.cc/t/i2c-e-pullup-resistor/129142/4
+  // Se l'I2C è a 5V puoi anche lasciarle attive, 30 k in parallelo a 4.7k hanno un effetto minimo, se l'I2C è a 3.3V allora devi disabilitare le pull up interne usando le due righe che ti ha postato PaoloP.
+  digitalWrite(SDA, 0);
+  digitalWrite(SCL, 0);
   // wait until serial port opens for native USB devices
   // Arduino Uno/Ethernet/Mega does not need it (no CDC serial)
   while (! Serial)
@@ -447,10 +459,18 @@ void setup()
   wdt_reset();
 
   // start the MQTT client
-  //mqttClient.begin(broker, net);
   mqttClient.begin(MQTTBROKER, net);
-  //mqttClient.begin("homeassistant.dmz.gonzaga.retaggio.net", net);
   mqttConnect();
+
+  // verify if the sensor is connected
+  // increase by 514 bytes the flash usage
+  /*
+  Wire.beginTransmission((uint8_t)M32Address);
+  if ( ! Wire.endTransmission() ) {
+    // use SRAM as the Flash is almost full, and after setup() is cleared.
+    //Serial.println("");
+  }
+  */
 
   // reset the watchdog
   wdt_reset();
@@ -460,9 +480,11 @@ void setup()
 void loop()
 {
   // variabled defined here and passed as reference to fetch_i2cdata
-  byte _status; // A two bit field indicating the status of the I2C read
+  //byte _status; // A two bit field indicating the status of the I2C read
   double pressure;
   double temperature;
+  uint16_t P_dec;
+  uint16_t T_dec;
   byte rawdata[4];
 
   // update the now variable
@@ -470,7 +492,7 @@ void loop()
   // temp debug to see if the messages are going through near the end of the uptime
   //now = 4294967000;
 
-  Serial.print(F("\r\n\nCurrent uptime: "));
+  Serial.print(F("\r\n\nuptime: "));
   Serial.println(now);
 
   // reset the watchdog
@@ -478,9 +500,16 @@ void loop()
 
 
   // get updated data from the sensor
-  _status = fetch_i2cdata(pressure, temperature, rawdata);
+  // function return not parsed
+  if ( fetch_i2cdata(pressure, temperature, P_dec, T_dec, rawdata) != 0 ) {
+    return;
+  }
+  // if return != 0, i2c failed (not anymore the original scope for the varuable)
+  wdt_reset();
 
-  _status = MQTTSend(pressure, temperature, rawdata);
+  // function return not parsed
+  //MQTTSend(pressure, temperature, P_dec, T_dec, rawdata);
   // watchdog to 8s
-  delay(5000-RUNTIME);
+  //delay(1000-RUNTIME);
+  delay(800);
 }
