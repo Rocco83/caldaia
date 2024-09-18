1429f5a102293218fd9470f82a5b90fcb0143626
Daniele Palumbo <daniele@retaggio.net>
2024-09-18 23:47:33 +0200

    Changed the MQTT address

--- boiler.h
+++ boiler.h
@@ -4,5 +4,5 @@
 #define MQTT_Password "byp6grotDetce"
 #define MQTT_topic_Message  "gonzaga/boiler"
 
-#define MQTTBROKER "homeassistant.dmz.gonzaga.retaggio.net" // FQDN of the MQTT broker
+#define MQTTBROKER "mqtt.gonzaga.retaggio.net" // FQDN of the MQTT broker
 #define MQTTPORT 1883
